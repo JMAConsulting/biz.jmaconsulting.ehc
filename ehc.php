dff11a52cfff2fd1af24168739904689f80b8a6f
root <root@environmentalhealth.org>
2019-01-23 23:13:05 -0800

    Report changes

--- ehc.php
+++ ehc.php
@@ -166,6 +166,25 @@ function getCustomColumnsByEntity($entity, $getID = FALSE) {
   return $customColumns;
 }
 
+function ehc_civicrm_pre($op, $objectName, $id, &$params) {
+  if ($op == 'create' && empty($id) && $objectName == 'Contribution') {
+    if (!empty($params['custom_256_-1']) ) {
+      $sc = CRM_Core_DAO::singleValueQuery("SELECT name FROM civicrm_option_value WHERE value = '" . $params['custom_256_-1'] . "' AND option_group_id = 96");
+      CRM_Core_Smarty::singleton()->assign('sc', $sc);
+    }
+    else {
+      CRM_Core_Smarty::singleton()->assign('sc', NULL);
+    }
+    if (!empty($params['custom_257_-1'])) {
+      $ssc = CRM_Core_DAO::singleValueQuery("SELECT name FROM civicrm_option_value WHERE value = '" . $params['custom_257_-1'] . "' AND option_group_id = 97");
+      CRM_Core_Smarty::singleton()->assign('ssc', $ssc);
+    }
+    else {
+      CRM_Core_Smarty::singleton()->assign('ssc', NULL);
+    }
+  }
+}
+
 /**
  * Implements hook_civicrm_alterSettingsFolders().
  *
@@ -203,7 +222,7 @@ function ehc_civicrm_post($op, $objectName, $objectId, &$objectRef) {
     }
     if ($objectName == "Contribution") {
       $contribution = civicrm_api3('Contribution', 'getsingle', array(
-        'return' => array("financial_type_id", "non_deductible_amount"),
+        'return' => array("financial_type_id", "non_deductible_amount", "custom_256"),
         'id' => $objectId,
       ));
       $org = civicrm_api3('EntityFinancialAccount', 'getsingle', array(
@@ -212,6 +231,16 @@ function ehc_civicrm_post($op, $objectName, $objectId, &$objectRef) {
         'entity_id' => $contribution["financial_type_id"],
         'options' => array('limit' => 1),
       ));
+      // Insert the recurring custom field value.
+      if (!empty($objectRef->contribution_recur_id)) {
+        civicrm_api3('Contribution', 'create', array('id' => $objectRef->id, 'custom_268' => 1, 'custom_269' => 'Recurring'));
+      }
+      else {
+        $ftName = CRM_Contribute_PseudoConstant::financialType($objectRef->financial_type_id);
+        $sc = CRM_Core_Smarty::singleton()->get_template_vars('sc');
+        $ssc = CRM_Core_Smarty::singleton()->get_template_vars('ssc');
+        civicrm_api3('Contribution', 'create', array('id' => $objectRef->id, 'custom_269' => getFinanceColumn($ftName, $sc, $ssc)));
+      }
       CRM_Core_Smarty::singleton()->assign("financialorg", $org["financial_account_id.contact_id.legal_name"]);
       CRM_Core_Smarty::singleton()->assign("nondeductibleamount", $contribution['non_deductible_amount']);
       if (!empty($objectRef->contribution_page_id)) {
@@ -241,6 +270,12 @@ function ehc_civicrm_post($op, $objectName, $objectId, &$objectRef) {
             foreach ($customColumns as $tableName => $keys) {
               $params['custom_' . $contriCustomIDs['Contribution']['sc']] = $value[$keys['sc']];
               $params['custom_' . $contriCustomIDs['Contribution']['ssc']] = $value[$keys['ssc']];
+
+              // Add finance team columns.
+              $ftName = CRM_Contribute_PseudoConstant::financialType($objectRef->financial_type_id);
+              $sc = CRM_Core_DAO::singleValueQuery("SELECT name FROM civicrm_option_value WHERE value = '" . $values[$keys['sc']] . "' AND option_group_id = 96");
+              $ssc = CRM_Core_DAO::singleValueQuery("SELECT name FROM civicrm_option_value WHERE value = '" . $values[$keys['ssc']] . "' AND option_group_id = 97");
+              $params['custom_269'] = getFinanceColumn($ftName, $sc, $ssc); 
             }
           }
           civicrm_api3('Contribution', 'create', $params);
@@ -248,6 +283,35 @@ function ehc_civicrm_post($op, $objectName, $objectId, &$objectRef) {
       }
     }
   }
+}
+
+function getFinanceColumn($ft, $sc, $ssc) {
+  switch($ft) {
+    case 'General':
+      if (strpos($sc, 'C4') !== false) {
+        $fc = 'EHJC';
+      }
+      elseif (in_array($ssc, ['Staff Donation', 'Recurring Monthly Donations'])) {
+        $fc = 'Recurring';
+      }
+    case 'Appeal':
+      if ((strpos($sc, 'Mid Year') !== false) || (strpos($sc, 'Year End') !== false) || in_array($ssc, ['Organizers efforts', 'Online Contribution', 'Mailer', 'Phone Banking'])) {
+        $fc = 'Individual';
+      }
+    case 'Special Events':
+      if (strpos($sc, 'C4') !== false) {
+        $fc = 'EHJC';
+      }
+      if (in_array($scc, ['Sponsor', 'Auction Item', 'In Kind'])) {
+        $fc = 'Sponsorship';
+      }
+      if (strpos($sc, 'Ticket') !== false) {
+        $fc = 'Events';
+      }
+    default:
+      break;
+  }
+  return $fc;
 }
 
 function ehc_civicrm_postProcess($formName, &$form) {
@@ -364,6 +428,10 @@ function ehc_civicrm_buildForm($formName, &$form) {
   elseif ($formName == 'CRM_Profile_Form_Edit') {
     CRM_Core_Resources::singleton()->addStyle('#editrow-custom_267 .label { display: none; }');
   }
+
+  if ($formName == 'CRM_Contribute_Form_Contribution' && $form->getVar('_formType') == 'AdditionalDetail') {
+    $form->setDefaults(['contribution_page_id' => 5]);
+  }
 }
 
 
@@ -391,6 +459,17 @@ function ehc_civicrm_alterMailParams(&$params, $context){
 function ehc_civicrm_alterReportVar($varType, &$var, &$object) {
   // Donation report
   if ($varType == 'columns' && get_class($object) == 'CRM_Report_Form_Contribute_Detail') {
+    $var['civicrm_contact']['fields'] += array(
+      'is_deceased' => array(
+        'title' => ts('Contact is deceased?'),
+      ),
+      'do_not_email' => array(
+        'title' => ts('Do Not Email'),
+      ),
+      'do_not_phone' => array(
+        'title' => ts('Do Not Call'),
+      ),
+    );
     $var['civicrm_contribution']['fields'] += array(
       'contribution_recur_id' => array(
         'title' => ts('Recurring Contribution?'),
@@ -402,5 +481,198 @@ function ehc_civicrm_alterReportVar($varType, &$var, &$object) {
         'title' => ts('Recurring Contributions'),
       ),
     );
+    $var['civicrm_contact']['filters'] += array(
+      'is_deceased' => array(
+        'title' => ts('Contact has deceased?'),
+      ),
+      'do_not_email' => array(
+        'title' => ts('Do Not Email'),
+      ),
+      'do_not_phone' => array(
+        'title' => ts('Do Not Phone'),
+      ),
+    );
+    $var['civicrm_contribution']['fields']['last_receive_date'] = [
+      'title' => ts('Last Gift Date'),
+      'dbAlias' => 'last_detail_temp.l_receive_date',
+    ];
+    $var['civicrm_contribution']['fields']['last_total_amount'] = [
+      'title' => ts('Last Gift Amount'),
+      'dbAlias' => 'last_detail_temp.l_total_amount',
+    ];
+    $var['civicrm_contribution']['fields']['last_financial_type_id'] = [
+      'title' => ts('Last Gift Financial Type'),
+      'dbAlias' => 'last_detail_temp.l_financial_type_id',
+    ];
+    $var['civicrm_contribution']['fields']['last_solicit_code'] = [
+      'title' => ts('Last Gift Solicit Code'),
+      'dbAlias' => 'last_detail_temp.l_solicit_code',
+    ];
+    $var['civicrm_contribution']['fields']['last_sub_solicit_code'] = [
+      'title' => ts('Last Gift Sub Solicit Code'),
+      'dbAlias' => 'last_detail_temp.l_sub_solicit_code',
+    ];
+    $var['civicrm_contribution']['fields']['last_contribution_id'] = [
+      'title' => ts('Last Contribution ID'),
+      'dbAlias' => 'last_detail_temp.l_id',
+    ];
+    $var['civicrm_contribution']['fields']['secondary_phone'] = [
+      'title' => ts('Donor Secondary Phone'),
+      'dbAlias' => 'p.phone SEPARATOR "<br/>\n"',
+    ];
+    $var['civicrm_contribution']['fields']['secondary_email'] = [
+      'title' => ts('Donor Secondary Email'),
+      'dbAlias' => 'e.email SEPARATOR "<br/>\n"',
+    ];
+    $var['civicrm_contribution']['fields']['sum_amount'] = [
+      'title' => ts('Aggregate Amount'),
+      'dbAlias' => 'agg_details_temp.sum_amount',
+    ];
+    $var['civicrm_contribution']['fields']['avg_amount'] = [
+      'title' => ts('Average Amount'),
+      'dbAlias' => 'agg_details_temp.avg_amount',
+    ];
+    $var['civicrm_contribution']['fields']['max_amount'] = [
+      'title' => ts('Largest Gift Amount'),
+      'dbAlias' => 'agg_details_temp.largest_gift_amount',
+    ];
+    $var['civicrm_contribution']['fields']['first_gift_date'] = [
+      'title' => ts('First Gift Date'),
+      'dbAlias' => 'agg_details_temp.first_gift_date',
+    ];
+    $var['civicrm_contribution']['fields']['number_of_donations'] = [
+      'title' => ts('Number of Donations'),
+      'dbAlias' => 'agg_details_temp.number_of_donations',
+    ];
+    $var['civicrm_contribution']['fields']['fy_amount'] = [
+      'title' => ts('Fiscal Year Amounts'),
+      'dbAlias' => 'fy_temp.fy_amounts',
+    ];
+    $var['civicrm_contribution']['fields']['cal_amount'] = [
+      'title' => ts('Calendar Year Amounts'),
+      'dbAlias' => 'cal_temp.cal_amounts',
+    ];
   }
+  if ($varType == 'sql' && get_class($object) == 'CRM_Report_Form_Contribute_Detail') {
+    $select  = $object->getVar('_select');
+    $select = str_replace('sum(contribution_civireport.total_amount) as civicrm_contribution_total_amount_sum', 'contribution_civireport.total_amount as civicrm_contribution_total_amount_sum', $select);
+    $select  = $object->setVar('_select', $select);
+    $from = $object->getVar('_from');
+    $where = $object->getVar('_where');
+    /* $sql = "CREATE TEMPORARY TABLE civireport_contribution_last_detail_temp DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci AS
+    SELECT * FROM (
+    SELECT now.id, last.id as l_id, last.receive_date FROM civicrm_contribution last 
+      LEFT JOIN
+        (SELECT contribution_civireport.id, contribution_civireport.contact_id,
+          contribution_civireport.receive_date
+         FROM civicrm_contact contact_civireport   
+      INNER JOIN civicrm_contribution contribution_civireport
+        ON contact_civireport.id = contribution_civireport.contact_id
+        AND contribution_civireport.is_test = 0
+      ) AS now ON now.contact_id = last.contact_id
+        AND now.id <> last.id AND now.receive_date > last.receive_date
+      WHERE now.id IS NOT NULL
+      ORDER BY now.id, last.receive_date DESC
+     ) as t
+     GROUP BY t.id";
+CRM_Core_Error::debug('ag', $sql); */
+   /* 
+    $sql = "CREATE TEMPORARY TABLE civireport_contribution_last_detail_temp DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci AS
+      SELECT
+        contact_id,
+        receive_date AS l_receive_date,
+        total_amount AS l_total_amount,
+        gd.solicit_code_256 AS l_solicit_code,
+        gd.sub_solicit_code_257 AS l_sub_solicit_code,
+        financial_type_id AS l_financial_type_id
+        FROM civicrm_contribution c1
+        LEFT JOIN civicrm_value_dp_gift_details_61 gd ON gd.entity_id = c1.id
+        WHERE receive_date = (SELECT max(receive_date) FROM civicrm_contribution c2 WHERE c2.contact_id = c1.contact_id)"; */
+    $sql = "
+    CREATE TEMPORARY TABLE civireport_contribution_last_detail_temp DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci AS
+    SELECT * FROM (select @i:=0) as a, (
+      SELECT now.id, last.id as l_id, last.receive_date AS l_receive_date, last.total_amount as l_total_amount, gd.solicit_code_256 AS l_solicit_code, gd.sub_solicit_code_257 AS l_sub_solicit_code, financial_type_id AS l_financial_type_id FROM civicrm_contribution last 
+      LEFT JOIN civicrm_value_dp_gift_details_61 gd ON gd.entity_id = last.id
+      LEFT JOIN
+        (SELECT contribution_civireport.id, contribution_civireport.contact_id,
+          contribution_civireport.receive_date
+        {$from}
+        {$where}
+      ) AS now ON now.contact_id = last.contact_id
+        AND now.id <> last.id AND now.receive_date > last.receive_date
+      WHERE now.id IS NOT NULL
+      ORDER BY now.id, last.receive_date DESC
+    ) AS t
+    GROUP BY t.id";
+    CRM_Core_DAO::executeQuery($sql);
+    $sql = "CREATE TEMPORARY TABLE civireport_contribution_aggregates_temp DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci AS SELECT c.contact_id, SUM(c.total_amount) as sum_amount, AVG(c.total_amount) as avg_amount, CONCAT('$ ', MAX(total_amount)) as largest_gift_amount, MIN(receive_date) as first_gift_date, COUNT(c.id) as number_of_donations FROM civicrm_contribution c WHERE c.contribution_status_id = 1 GROUP BY c.contact_id";
+    CRM_Core_DAO::executeQuery($sql);
+    $sql = "CREATE TEMPORARY TABLE civireport_contribution_fy_temp DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci AS SELECT c.contact_id, SUM(c.total_amount) as fy_amount FROM civicrm_contribution c WHERE c.contribution_status = 1 AND c.receive_date BETWEEN ";
+    $sql = "CREATE TEMPORARY TABLE civireport_contribution_fy_temp DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci AS SELECT
+   CASE WHEN MONTH(receive_date)>=7 THEN
+          concat(YEAR(receive_date), '-',YEAR(receive_date)+1)
+   ELSE concat(YEAR(receive_date)-1,'-', YEAR(receive_date)) END AS financial_year,
+   SUM(total_amount) as total_amount_sum, contact_id
+FROM civicrm_contribution WHERE contribution_status_id = 1
+GROUP BY contact_id, financial_year";
+    CRM_Core_DAO::executeQuery($sql);
+    $sql = "CREATE TEMPORARY TABLE civireport_contribution_join_temp DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci AS
+      SELECT GROUP_CONCAT(concat(financial_year, ' - $ ', total_amount_sum) SEPARATOR '<br/>\n') as fy_amounts, contact_id FROM civireport_contribution_fy_temp group by contact_id
+    ";
+    CRM_Core_DAO::executeQuery($sql);
+    $sql = "CREATE TEMPORARY TABLE civireport_contribution_cal_temp DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci AS SELECT
+          YEAR(receive_date) AS cal_year,
+   SUM(total_amount) as total_amount_sum, contact_id
+FROM civicrm_contribution WHERE contribution_status_id = 1
+GROUP BY contact_id, cal_year";
+    CRM_Core_DAO::executeQuery($sql);
+    $sql = "CREATE TEMPORARY TABLE civireport_contribution_cal_join_temp DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci AS
+      SELECT GROUP_CONCAT(concat(cal_year, ' - $ ', total_amount_sum) SEPARATOR '<br/>\n') as cal_amounts, contact_id FROM civireport_contribution_cal_temp group by contact_id
+    ";
+    CRM_Core_DAO::executeQuery($sql);
+    
+    //$_aclFrom = " LEFT JOIN civireport_contribution_last_detail_temp last_detail_temp ON last_detail_temp.contact_id = contribution_civireport.contact_id ";
+    $_aclFrom = " LEFT JOIN civireport_contribution_last_detail_temp last_detail_temp ON last_detail_temp.id = contribution_civireport.id 
+      LEFT JOIN civireport_contribution_aggregates_temp agg_details_temp ON agg_details_temp.contact_id = contribution_civireport.contact_id
+      LEFT JOIN civireport_contribution_join_temp fy_temp ON fy_temp.contact_id = contribution_civireport.contact_id
+      LEFT JOIN civireport_contribution_cal_join_temp cal_temp ON cal_temp.contact_id = contribution_civireport.contact_id ";
+    $from .= $_aclFrom;
+    $from .= " LEFT JOIN civicrm_phone p ON p.contact_id = contact_civireport.id AND p.is_primary <> 1";
+    $from .= " LEFT JOIN civicrm_email e ON e.contact_id = contact_civireport.id AND e.is_primary <> 1 AND e.email <> email_civireport.email";
+    $object->setACLFromForLastContribution = $object->getVar('_aclFrom');
+    $aclFrom = $object->getVar('_aclFrom') . $_aclFrom;
+    
+    $object->setVar('_from', $from);
+    $object->setVar('_aclFrom', $aclFrom);
+    $object->setFromForLastContribution = $from;
+  }
+  
+  if ($varType == 'rows' && get_class($object) == 'CRM_Report_Form_Contribute_Detail') {
+    $object->setVar('_aclFrom', $object->setACLFromForLastContribution);
+    $object->setVar('_from', $object->setFromForLastContribution);
+    $contributionTypes = CRM_Contribute_PseudoConstant::financialType();
+    $solicit = CRM_Core_OptionGroup::values('solicit_code');
+    $subsolicit = CRM_Core_OptionGroup::values('sub_solicit_code');
+    foreach ($var as $key => $row) {
+      if ($value = CRM_Utils_Array::value('civicrm_contribution_last_financial_type_id', $row)) {
+        $var[$key]['civicrm_contribution_last_financial_type_id'] = $contributionTypes[$value];
+      }
+      if ($value = CRM_Utils_Array::value('civicrm_contribution_last_solicit_code', $row)) {
+        $var[$key]['civicrm_contribution_last_solicit_code'] = $solicit[$value];
+      }
+      if ($value = CRM_Utils_Array::value('civicrm_contribution_last_sub_solicit_code', $row)) {
+        $var[$key]['civicrm_contribution_last_sub_solicit_code'] = $subsolicit[$value];
+      }
+      if ($value = CRM_Utils_Array::value('civicrm_contribution_last_total_amount', $row)) {
+        $var[$key]['civicrm_contribution_last_total_amount'] = CRM_Utils_Money::format($value, $row['civicrm_contribution_currency']);
+      } 
+      if ($value = CRM_Utils_Array::value('civicrm_contribution_sum_amount', $row)) {
+        $var[$key]['civicrm_contribution_sum_amount'] = CRM_Utils_Money::format($value, $row['civicrm_contribution_currency']);
+      }
+      if ($value = CRM_Utils_Array::value('civicrm_contribution_avg_amount', $row)) {
+        $var[$key]['civicrm_contribution_avg_amount'] = CRM_Utils_Money::format($value, $row['civicrm_contribution_currency']);
+      }
+    }
+  }
+  
 }
